074bc31d502d636a9d384bf4e177be488e5afa6c
Иван <vanek.potemkin@gmail.com>
2024-02-11 21:15:27 +0300

    Создал Spring Boot проект.

---
+++ src/main/java/pro/sky/shelterpettelegrambot/ShelterPetTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.shelterpettelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelterPetTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShelterPetTelegramBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/shelterpettelegrambot/ShelterPetTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.shelterpettelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelterPetTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
